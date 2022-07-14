21440fb5283848f2f087638610e4f1df775bac3b
neon-sunset <neon-sunset@outlook.com>
2022-07-15 01:46:26 +0300

    Initial commit

---
+++ RangeExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace RangeExtensions.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ RangeExtensions/RangeExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace RangeExtensions;
+
+public class RangeExtensions
+{
+
+}
