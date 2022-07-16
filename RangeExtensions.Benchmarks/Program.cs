c4d722a5ade021411bd575fc1b648b7ea6889988
neon-sunset <neon_sunset@outlook.com>
2022-07-16 16:02:53 +0300

    Remove accidental changes

--- RangeExtensions.Benchmarks/Program.cs
+++ RangeExtensions.Benchmarks/Program.cs
@@ -1,10 +1,5 @@
-﻿//using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
-//BenchmarkSwitcher
-//   .FromAssembly(typeof(Program).Assembly)
-//   .Run();
-
-foreach (var i in (0..10).AsEnumerable().Reverse())
-{
-    Console.WriteLine(i);
-}
+BenchmarkSwitcher
+  .FromAssembly(typeof(Program).Assembly)
+  .Run();
