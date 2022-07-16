3a084a41c63a8abe1fec4013dbeb150099c65578
neon-sunset <neon-sunset@outlook.com>
2022-07-16 16:58:33 +0300

    More tests

--- RangeExtensions.Tests/RangeEnumerable.cs
+++ RangeExtensions.Tests/RangeEnumerable.cs
@@ -18,8 +18,8 @@ public class RangeEnumerableTests
     [InlineData(177013)]
     public void AscendingRangeOutput_MatchesEnumerableRange(int count)
     {
-        var enumerable = Enumerable.Range(0, count);
         var range = (0..count).AsEnumerable();
+        var enumerable = Enumerable.Range(0, count);
 
         Assert.Equal(enumerable, range);
     }
@@ -28,12 +28,52 @@ public class RangeEnumerableTests
     [InlineData(0)]
     [InlineData(10)]
     [InlineData(177013)]
+    public void AscendingRangeEnumeratorOutput_MatchesEnumerableRange(int count)
+    {
+        var range = 0..count;
+        var enumerable = Enumerable.Range(0, count);
+        
+        IEnumerable<int> Enumerate()
+        {
+            foreach (var i in range)
+            {
+                yield return i;
+            }
+        }
+
+        Assert.Equal(enumerable, Enumerate());
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(10)]
+    [InlineData(177013)]
     public void DescendingRangeOutput_MatchesEnumerableRange(int count)
     {
-        var enumerable = Enumerable.Range(0, count).Reverse();
         var range = (count..0).AsEnumerable();
+        var enumerable = Enumerable.Range(0, count).Reverse();
 
         Assert.Equal(enumerable, range);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(10)]
+    [InlineData(177013)]
+    public void DescendingRangeEnumeratorOutput_MatchesEnumerableRange(int count)
+    {
+        var range = count..0;
+        var enumerable = Enumerable.Range(0, count).Reverse();
+
+        IEnumerable<int> Enumerate()
+        {
+            foreach (var i in range)
+            {
+                yield return i;
+            }
+        }
+
+        Assert.Equal(enumerable, Enumerate());
     }
 
     [Theory]
