268c3aefe3cfbc602a616f8574f22b73a79969ec
neon-sunset <neon-sunset@outlook.com>
2022-07-16 16:49:54 +0300

    Add more tests

--- RangeExtensions.Tests/RangeEnumerable.cs
+++ RangeExtensions.Tests/RangeEnumerable.cs
@@ -2,21 +2,49 @@ namespace RangeExtensions.Tests;
 
 public class RangeEnumerableTests
 {
-    [Fact]
-    public void AscendingRangeOutput_MatchesEnumerableRange()
+    private static IEnumerable<object[]> InvalidRanges()
     {
-        var enumerable = Enumerable.Range(0, 10);
-        var range = (0..10).AsEnumerable();
+        yield return new object[] { 0..^10 };
+        yield return new object[] { ^5..10 };
+        yield return new object[] { ^5..^10 };
+        yield return new object[] { ^5..^10 };
+        yield return new object[] { 0.. };
+        yield return new object[] { 10.. };
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(10)]
+    [InlineData(177013)]
+    public void AscendingRangeOutput_MatchesEnumerableRange(int count)
+    {
+        var enumerable = Enumerable.Range(0, count);
+        var range = (0..count).AsEnumerable();
 
         Assert.Equal(enumerable, range);
     }
 
-    [Fact]
-    public void DescendingRangeOutput_MatchesEnumerableRange()
+    [Theory]
+    [InlineData(0)]
+    [InlineData(10)]
+    [InlineData(177013)]
+    public void DescendingRangeOutput_MatchesEnumerableRange(int count)
     {
-        var enumerable = Enumerable.Range(0, 10).Reverse();
-        var range = (10..0).AsEnumerable();
+        var enumerable = Enumerable.Range(0, count).Reverse();
+        var range = (count..0).AsEnumerable();
 
         Assert.Equal(enumerable, range);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidRanges))]
+    public void RangeEnumerable_ThrowsOnInvalidRange(Range range)
+    {
+        void AsEnumerable()
+        {
+            _ = range.AsEnumerable();
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(AsEnumerable);
     }
 }
